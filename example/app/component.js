dd2fb6d63d42f038aa7a8850e46bcb118b3929c0
Dima <some@mail>
2018-05-14 12:50:52 +0400

    9  добавление формы в приложение

--- example/app/component.js
+++ example/app/component.js
@@ -10,9 +10,11 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 var core_1 = require("@angular/core");
 var repository_model_1 = require("./repository.model");
+var product_model_1 = require("./product.model");
 var ProductComponent = (function () {
     function ProductComponent() {
         this.model = new repository_model_1.Model();
+        this.newProduct = new product_model_1.Product();
     }
     ProductComponent.prototype.getProduct = function (key) {
         return this.model.getProduct(key);
@@ -20,8 +22,15 @@ var ProductComponent = (function () {
     ProductComponent.prototype.getProducts = function () {
         return this.model.getProducts();
     };
-    ProductComponent.prototype.getSelected = function (product) {
-        return product.name == this.selectedProduct;
+    Object.defineProperty(ProductComponent.prototype, "jsonProduct", {
+        get: function () {
+            return JSON.stringify(this.newProduct);
+        },
+        enumerable: true,
+        configurable: true
+    });
+    ProductComponent.prototype.addProduct = function (p) {
+        console.log("New Product: " + this.jsonProduct);
     };
     ProductComponent = __decorate([
         core_1.Component({
